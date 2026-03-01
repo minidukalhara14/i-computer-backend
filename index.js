9f43684b0489684de3875ac776f6e7b9ed4f3bf4
minidukalhara2@gmail.com <minidukalhara14@gmail.com>
2026-03-01 12:01:42 +0530

    fixed the student route issue ❤️‍🩹

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import studentRouter from "./routers/StudentRouter.js";
 import userRouter from "./routers/userRouter.js";
 import authentication from "./middleweres/authentication.js";
 import productRouter from "./routers/productRouter.js";
@@ -21,7 +20,6 @@ app.use(express.json()); //midle were pluged
 
 app.use(authentication)
 
-app.use("/students" , studentRouter)
 app.use("/users" , userRouter)
 app.use("/products" ,productRouter )
  
